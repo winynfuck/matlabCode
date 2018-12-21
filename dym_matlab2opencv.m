8850e7eba6f14e043039e4086422d0032ffbf64a
winynfuck <38125063+winynfuck@users.noreply.github.com>
2018-12-21 12:28:35 +0800

    Update dym_matlab2opencv.m

--- dym_matlab2opencv.m
+++ dym_matlab2opencv.m
@@ -1,5 +1,5 @@
 function dym_matlab2opencv(variable, fileName, flag, varClass)
- 
+%此函数用于将matlab内的变量转换伟yml数据库文件，然后可以在opencv中读取yml数据库文件。运行环境是matlba.即先转换后调用文件。
 % varClass: the variable class waiting for write:
 %           'i': for int
 %           'f': for float
